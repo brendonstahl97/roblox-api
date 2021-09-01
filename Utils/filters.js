efd3e5ab4c3fa8d0b9426ccc333fcabb93547b56
brendonstahl97 <brendonstahl97@gmail.com>
2021-09-01 00:25:17 -0400

    implemented concurrent batch searching

--- Utils/filters.js
+++ Utils/filters.js
@@ -1,5 +1,6 @@
 const noblox = require('noblox.js');
 
+//generate a random place ID based on Roblox criteria
 const generatePlaceId = () => {
     const placeId = Math.floor(Math.random() * 7317738139);
     if (placeId >= 3000) {
@@ -9,12 +10,15 @@ const generatePlaceId = () => {
     };
 };
 
+//Get the data for a specified number of places via concurrent asynchronous api calls. !!!VERY FAST!!!
 const getMassPlaceData = async (numPlaces) => {
+    //Create holding array and generate specified number of placeIds
     let placeIds = [];
     for (let i = 0; i < numPlaces; i++) {
         placeIds.push(generatePlaceId());
     };
 
+    //Iterate through IDs and get corresponding data from API
     const placeData = placeIds.map(async (id) => {
         try {
             const data = await noblox.getPlaceInfo(id);
@@ -24,9 +28,11 @@ const getMassPlaceData = async (numPlaces) => {
         }
     });
 
+    //Ensure all promises are resolved concurrently
     return Promise.all(placeData);
 };
 
+//Check validity of the view count of a place based on specified max and min view count
 const checkVisit = (data, min = 1, max = 100000000) => {
     if (data.VisitedCount >= min && data.VisitedCount < max) {
         return true;
@@ -34,6 +40,7 @@ const checkVisit = (data, min = 1, max = 100000000) => {
     return false;
 };
 
+//Check to ensure place has been updated at least one time
 const checkUpdate = ({ Created, Updated }) => {
     const created = new Date(Created);
     const updated = new Date(Updated);
@@ -44,78 +51,86 @@ const checkUpdate = ({ Created, Updated }) => {
     return false;
 };
 
+//Apply all filters to data
 const checkPlaceValidity = (rawData, visitFilter = 0, dateFilter = false) => {
     const data = rawData;
     let visitVerified = false;
     let dateVerified = false;
 
-    if (visitFilter == 1) {
-        if (checkVisit(data, 1, 1000)) {
-            visitVerified = true;
-            console.log("Found a game with visits between 1 and 10000");
+    //ensure data was actually returned
+    if (data != null) {
+        //Visit filter checks
+        if (visitFilter == 1) {
+            if (checkVisit(data, 1, 1000)) {
+                visitVerified = true;
+                console.log("Found a game with visits between 1 and 10000");
+            };
+        } else if (visitFilter == 2) {
+            if (checkVisit(data, 1000, 10000)) {
+                visitVerified = true;
+                console.log("Found a game with visits between 10000 and 1000000");
+            };
+        } else if (visitFilter == 3) {
+            if (checkVisit(data, 10000)) {
+                visitVerified = true;
+                console.log("Found a game with visits between 10000 and Big Chungus");
+            };
+        } else {
+            if (checkVisit(data)) {
+                visitVerified = true
+                console.log("Found a game with more than 1 visit!");
+            };
         };
-    } else if (visitFilter == 2) {
-        if (checkVisit(data, 1000, 10000)) {
-            visitVerified = true;
-            console.log("Found a game with visits between 10000 and 1000000");
-        };
-    } else if (visitFilter == 3) {
-        if (checkVisit(data, 10000)) {
-            visitVerified = true;
-            console.log("Found a game with visits between 10000 and Big Chungus");
-        };
-    } else {
-        if (checkVisit(data)) {
-            visitVerified = true
-            console.log("Found a game with more than 1 visit!");
+
+        //Date filter checks
+        if (dateFilter) {
+            if (checkUpdate(data)) {
+                dateVerified = true;
+                console.log('Found game that has been updated');
+            };
+        } else { dateVerified = true };
+
+        //Final check to see if all filter criteria are met
+        if (visitVerified && dateVerified) {
+            return true;
         };
     };
-
-    if (dateFilter) {
-        if (checkUpdate(data)) {
-            dateVerified = true;
-            console.log('Found game that has been updated');
-        };
-    } else { dateVerified = true };
-
-    if (visitVerified && dateVerified) {
-        return true;
-    }
     return false;
 };
 
+
 const filters = {
-
-    test: async () => {
-        const data = await getMassPlaceData(50);
-        console.log(data);
-    },
-
+    //Get general place info from a given placeID
     getPlaceInfo: async (placeId) => {
         const data = await noblox.getPlaceInfo(placeId)
         return data;
     },
 
+    //Find an eligable place based on specified filter info
+    //VisitFilter: 0 = >1 view, 1 = between 1 and 1000 views, 2 = between 1000 and 10000 views, 3 = >10000 views
+    //dateFilter: false = ignore update, true = ensure game has at least one update
     getPlace: async (visitFilter = 0, dateFilter = false) => {
+        //bakcup filter data for recursion
         const backupVisit = visitFilter;
         const backupDate = dateFilter;
-        const placeId = generatePlaceId();
+        const massPlaceData = await getMassPlaceData(50);
+        let verifiedPlace = null;
 
-        try {
-            const data = await noblox.getPlaceInfo(placeId);
-            const isValid = checkPlaceValidity(data, visitFilter, dateFilter);
-
-            if (isValid) {
-                console.log("Place is Valid! Nice Work!");
-                return placeId;
-            } else {
-                console.log("Checking for a new place, That one sucked.");
-                return await filters.getPlace(backupVisit, backupDate);
-            };
-
-        } catch (error) {
-            return await filters.getPlace(backupVisit, backupDate);
+        //Iterate through all retreived places and determine if there is a valid place within the bunch
+        for (let i = 0; i < massPlaceData.length; i++) {
+            const isValid = checkPlaceValidity(massPlaceData[i], visitFilter, dateFilter);
+            if (isValid) { 
+                console.log(`${massPlaceData[i].AssetId} is a verified place!`);
+                verifiedPlace = massPlaceData[i].AssetId;
+                break;
+            }
         };
+
+        if(verifiedPlace != null) {
+            return verifiedPlace;
+        };
+
+        return await filters.getPlace(backupVisit, backupDate);
     }
 };
 
